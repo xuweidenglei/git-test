85747ba0f3d459d98dba061a68c3377f2b3037d0
jawen <1874975773@qq.com>
2021-11-04 15:13:02 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("Hell Git3! hot-fix");
         System.out.println("Hell Git4! master test");
         System.out.println("Hell Git5! hot-fix test");
+        System.out.println("push test");
     }
 }
