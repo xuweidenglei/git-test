07a3f4d85bad722f0ec56d1b2b968bf72098523c
jawen <1874975773@qq.com>
2021-11-04 13:54:37 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hell Git!");
+        System.out.println("Hell Git2!");
     }
 }
