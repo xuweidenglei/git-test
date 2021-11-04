73ddeb6b2e7e6b0a4e3f9de8683d0b973afdd980
xuweidenglei <91776697+xuweidenglei@users.noreply.github.com>
2021-11-04 15:17:11 +0800

    Update GitTest.java
    pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Hell Git4! master test");
         System.out.println("Hell Git5! hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
