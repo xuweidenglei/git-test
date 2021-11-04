0ee912b735ac5b868861b527996b00d9b49c95b0
jawen <1874975773@qq.com>
2021-11-04 14:45:07 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hell Git!");
         System.out.println("Hell Git2!");
+        System.out.println("Hell Git3! hot-fix");
     }
 }
