a1a458fa6f715c4a8d9d02b07fd6c654a2427973
jawen <1874975773@qq.com>
2021-11-04 15:36:19 +0800

    push test second

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hell Git5! hot-fix test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("push test second");
     }
 }
